85241e1beb52173a9c4e0c6b9ac11662ddd4ad69
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-11-25 14:34:37 -0500

    updated text

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n subject: $subject \n  Message: $message";
-$recipient = "admin@themepure.net";
+$recipient = "mredwinrodriguez@gmail.com";
 $subject = "Add Your Subject Here";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
